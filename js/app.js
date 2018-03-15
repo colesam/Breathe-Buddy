4c40593826e9ecd4db296971d42076d44c8004f3
invert <matthew.schlichting@gmail.com>
2018-03-15 13:43:58 -0500

    added init stuff as well as a basic call to the air quality API

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var airQuality = angular.module("airQuality", ['ui.router']);
+var airQuality = angular.module("airQuality", []);
 
-airQuality.controller("appCtrl", function($scope) {
+airQuality.controller("appCtrl", function($scope, $http) {
 
   $scope.init = function (){
       console.log('Version 0.0.1')
--- js/controllers/airQualityController.js
+++ js/controllers/airQualityController.js
@@ -1,7 +1,81 @@
 /**
  * Created by Mathew on 3/9/2018.
  */
-function airQualityControllerFunction($scope) {
+function airQualityControllerFunction($scope, $http) {
+
+    $scope.airQualityHome = 'https://api.openaq.org/v1/';
+    $scope.measurements = 'measurements';
+
+
+
+    $scope.init = function (){
+        $scope.getAirQuality(undefined, undefined, undefined, 'co', {latitude: 43.34,  longitude: 23.04}, 2500, undefined, undefined, undefined, undefined, 5);
+    };
+
+
+
+    /*
+        Information about the API can be found at https://docs.openaq.org/#api-Measurements-GetMeasurements
+
+        string      country
+            Limit results by a certain country.
+        string      city
+            Limit results by a certain city.
+        string      location
+            Limit results by a certain location.
+        string      parameter
+            Limit to certain one or more parameters (ex. parameter=pm25 or parameter[]=co&parameter[]=pm25)
+            Allowed values: pm25, pm10, so2, no2, o3, co, bc
+        string      coordinates
+            Center point (lat, lon) used to get measurements within a certain area. (ex. coordinates=40.23,34.17)
+        number      radius
+            Radius (in meters) used to get measurements within a certain area, must be used with coordinates.
+            Default value: 2500
+        number      valueFrom
+            Show results above value threshold, useful in combination with parameter.
+        number      valueTo
+            Show results below value threshold, useful in combination with parameter.
+        string      dateFrom
+            Show results after a certain date. This acts on the utc timestamp of each measurement. (ex. 2015-12-20, or 2015-12-20T09:00:00)
+        string      dateTo
+            Show results before a certain date. This acts on the utc timestamp of each measurement. (ex. 2015-12-20, or 2015-12-20T09:00:00)
+        number      limit
+            Change the number of results returned, max is 10000.
+            Default value: 100
+    */
+
+    $scope.getAirQuality = function(country, city, location, parameter, coordinates, radius, valueFrom, valueTo, dateFrom, dateTo, limit) {
+        $http({
+            method: 'GET',
+            url: $scope.airQualityHome + $scope.measurements,
+            parameter:  {
+                            country:        country,
+                            city:           city,
+                            location:       location,
+                            parameter:      parameter,
+                            coordinates:    coordinates,
+                            radius:         radius,
+                            value_from:     valueFrom,
+                            value_to:       valueTo,
+                            date_from:      dateFrom,
+                            date_to:        dateTo,
+                            limit:          limit
+                        }
+        }).then(function successCallback(response) {
+
+            console.log(response.data.results);
+
+
+
+        }, function errorCallback(response) {
+            console.log('Failed to obtained air quality data!')
+        });
+    };
+
+
+
+
+
 
 
 
