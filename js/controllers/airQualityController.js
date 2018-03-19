cf307ace4315fbff9d088f90379460e6ec96bc14
invert <matthew.schlichting@gmail.com>
2018-03-19 12:29:38 -0500

    now has clusters and updates with location of current map

--- js/controllers/airQualityController.js
+++ js/controllers/airQualityController.js
@@ -11,6 +11,7 @@ function airQualityControllerFunction($scope, $http) {
     $scope.API_KEY = 'AIzaSyAa9M8srClYjpe9v5kURZ9JEM1Vg3H0nNQ';
     $scope.API_LOC = 'https://www.google.com/maps/embed/v1/place';
     $scope.API_START = $scope.API_LOC + '?key=' + $scope.API_KEY + '&q=';
+    $scope.MARKER_CLUSTER_OBJ = {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'};
 
     /* User input */
     $scope.location = '';
@@ -22,15 +23,34 @@ function airQualityControllerFunction($scope, $http) {
     $scope.markers = [];
     $scope.geocoder = null;
     $scope.fullscreen = false;
+    $scope.markerCluterManager = null;
+    $scope.loading = false;
 
 
 
     $scope.init = function (){
         $scope.mapInit();
+    };
 
-        $scope.getAirQuality(undefined, undefined, undefined, undefined, '17.6599188,75.9063906', 2500, undefined, undefined, undefined, undefined, undefined, function(results){
-            console.log(results);
-        });
+
+    $scope.updateClusters = function(){
+        $scope.markerCluterManager = new MarkerClusterer($scope.map, $scope.markers, $scope.MARKER_CLUSTER_OBJ);
+        $scope.updateLocation();
+    };
+
+    $scope.populateMarkers = function(data){
+        //clear old markers
+        $scope.markers = [];
+
+        for(var i=0; i<data.length; i++){
+            if(data[i] !== undefined){
+                if(data[i].coordinates !== undefined){
+                    $scope.placeMarker({lat: data[i].coordinates.latitude, lng: data[i].coordinates.longitude});
+                }
+            }
+        }
+
+        $scope.updateClusters();
 
     };
     
@@ -56,10 +76,37 @@ function airQualityControllerFunction($scope, $http) {
         }
     };
 
+
+    $scope.getRadius = function(bounds){
+        // r = radius of the earth in km
+        var r = 6378.8;
+
+        // degrees to radians (divide by 57.2958)
+        var ne_lat = bounds.getNorthEast().lat() / 57.2958;
+        var ne_lng = bounds.getNorthEast().lng() / 57.2958;
+        var c_lat = bounds.getCenter().lat() / 57.2958;
+        var c_lng = bounds.getCenter().lng() / 57.2958;
+
+        // distance = circle radius from center to Northeast corner of bounds
+        var r_km = r * Math.acos(
+            Math.sin(c_lat) * Math.sin(ne_lat) +
+            Math.cos(c_lat) * Math.cos(ne_lat) * Math.cos(ne_lng - c_lng)
+        );
+
+        return r_km *1000 // radius in meters
+    };
+
+
+    $scope.updateMap = function(){
+        //get radius and set max radius at 10,000
+        var radius = $scope.getRadius($scope.map.getBounds());
+        radius = Math.round(Math.min(radius, 100000));
+        $scope.getAirQuality($scope.populateMarkers, $scope.lat_lng, radius , undefined, '2018-3-18', '2018-3-19', 10000);
+    };
+
     $scope.updateLocation = function(){
-        $scope.$apply(function(){
-            $scope.lat_lng = (Math.round($scope.map.center.lat() * 1000) / 1000) + ', ' + (Math.round($scope.map.center.lng() * 1000) / 1000);
-        });
+        $scope.loading = false;
+        $scope.lat_lng = (Math.round($scope.map.center.lat() * 1000) / 1000) + ', ' + (Math.round($scope.map.center.lng() * 1000) / 1000);
     };
 
     $scope.submitLocation = function(event){
@@ -98,10 +145,9 @@ function airQualityControllerFunction($scope, $http) {
         });
 
         $scope.geocoder = new google.maps.Geocoder;
-
         $scope.lat_lng = latitude + ', ' + longitude;
 
-        $scope.map.center_changed = $scope.updateLocation;
+        google.maps.event.addListener($scope.map, 'idle', $scope.updateMap);
         $scope.map.bounds_changed = $scope.checkFullScreen;
     };
 
@@ -111,12 +157,13 @@ function airQualityControllerFunction($scope, $http) {
         coordinates = {lat: ####, lng: ####};
      */
     $scope.placeMarker = function(coordinates){
-        $scope.markers.push(new google.maps.Marker({
-            position: coordinates,
-            map: $scope.map
-        }));
+        if(coordinates.lng !== undefined && coordinates.lat !== undefined) {
+            $scope.markers.push(new google.maps.Marker({
+                position: coordinates,
+                map: $scope.map
+            }));
+        }
     };
-
 
 
     $scope.convertNameToAPI = function(s){
@@ -131,9 +178,6 @@ function airQualityControllerFunction($scope, $http) {
                 result += s.charAt(i);
             }
         }
-
-
-
         return result;
     };
 
@@ -141,53 +185,58 @@ function airQualityControllerFunction($scope, $http) {
     /*
         Information about the API can be found at https://docs.openaq.org/#api-Measurements-GetMeasurements
 
-        string      country
-            Limit results by a certain country.
-        string      city
-            Limit results by a certain city.
-        string      location
-            Limit results by a certain location.
-        string      parameter
-            Limit to certain one or more parameters (ex. parameter=pm25 or parameter[]=co&parameter[]=pm25)
-            Allowed values: pm25, pm10, so2, no2, o3, co, bc
         string      coordinates
             Center point (lat, lon) used to get measurements within a certain area. (ex. coordinates=40.23,34.17)
+
         number      radius
             Radius (in meters) used to get measurements within a certain area, must be used with coordinates.
             Default value: 2500
-        number      valueFrom
-            Show results above value threshold, useful in combination with parameter.
-        number      valueTo
-            Show results below value threshold, useful in combination with parameter.
+
+        string      parameter
+            Limit to certain one or more parameters (ex. parameter=pm25 or parameter[]=co&parameter[]=pm25)
+            Allowed values: pm25, pm10, so2, no2, o3, co, bc
+
         string      dateFrom
             Show results after a certain date. This acts on the utc timestamp of each measurement. (ex. 2015-12-20, or 2015-12-20T09:00:00)
+
         string      dateTo
             Show results before a certain date. This acts on the utc timestamp of each measurement. (ex. 2015-12-20, or 2015-12-20T09:00:00)
+
         number      limit
             Change the number of results returned, max is 10000.
             Default value: 100
     */
 
-    $scope.getAirQuality = function(country, city, location, parameter, coordinates, radius, valueFrom, valueTo, dateFrom, dateTo, limit, callBack) {
-        console.log('Getting data...');
+    $scope.getAirQuality = function(callBack, coordinates, radius, type, dateFrom, dateTo, limit) {
+        var parameters = {};
+        if(coordinates !== undefined){
+            parameters.coordinates = coordinates;
+        }
+        if(radius !== undefined){
+            parameters.radius = radius;
+        }
+        if(type !== undefined){
+            parameters.parameter = type;
+        }
+        if(dateFrom !== undefined){
+            parameters.date_from = dateFrom;
+        }
+        if(dateTo !== undefined){
+            parameters.date_to = dateTo;
+        }
+        if(limit !== undefined){
+            parameters.limit = limit
+        }
+
+        $scope.loading = true;
+        $scope.lat_lng = 'Loading...';
+
         $http({
             method: 'GET',
             url: $scope.airQualityHome + $scope.measurements,
-            params:  {
-                            //country:        country,
-                            //city:           city,
-                            //location:       location,
-                            //parameter:      parameter,
-                            coordinates:     coordinates,
-                            radius:         radius//,
-                            //value_from:     valueFrom,
-                            //value_to:       valueTo,
-                            //date_from:      dateFrom,
-                            //date_to:        dateTo,
-                            //limit:          limit
-                        }
+            params:  parameters
         }).then(function successCallback(response) {
-            callBack(response);
+            callBack(response.data.results);
         }, function errorCallback(response) {
             console.log('Failed to obtained air quality data!')
         });
@@ -203,7 +252,3 @@ function airQualityControllerFunction($scope, $http) {
 }
 
 airQuality.controller('airQualityCtrl', airQualityControllerFunction);
-
-function initMap(){
-    console.log('in here');
-}
