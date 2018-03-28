cfd8cd22d0c259f8cb959d278e8787f9bd06e44f
Samuel Cole <cole.samuel21@gmail.com>
2018-03-28 18:01:57 +0000

    started about-page

--- js/app.js
+++ js/app.js
@@ -4,9 +4,38 @@ var airQuality = angular.module("airQuality", []);
 
 airQuality.controller("appCtrl", function($scope, $http) {
 
-  $scope.init = function (){
-      console.log('Version 0.0.1')
+    $scope.init = function (){
+        console.log('Version 0.0.1')
+    };
 
-  };
+});
 
+// NAV JS
+
+$(document).ready(function() {
+    
+    $('#app-nav').click(() => {
+        
+        //  update nav css
+        $('#app-nav').addClass('active');
+        $('#about-nav').removeClass('active');
+        
+        //  display proper html
+        $('#app-html').removeClass('d-none');
+        $('#about-html').addClass('d-none');
+        
+    });
+    
+    $('#about-nav').click(() => {
+        
+        //  update nav css
+        $('#about-nav').addClass('active');
+        $('#app-nav').removeClass('active');
+        
+        //  display proper html
+        $('#about-html').removeClass('d-none');
+        $('#app-html').addClass('d-none');
+        
+    });
+    
 });
